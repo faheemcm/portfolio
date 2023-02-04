603b56af3987b7a5d47e11390207bf024d91b53a
faheemcm <itsfaheemcm@gmail.com>
2023-02-04 11:16:09 +0530

    initial

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        items:4,
+        loop:true,
+        margin:10,
+        nav:false,
+        autoplay:true,
+    })
+})
